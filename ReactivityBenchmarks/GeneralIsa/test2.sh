609f7a61bef6635c6fa8e15c736d0a33e3a62fcd
Florian Myter <fmyter@vub.ac.be>
2018-05-22 17:26:05 +0200

    Testing Raspberry usecase

---
+++ ReactivityBenchmarks/GeneralIsa/test2.sh
@@ -0,0 +1,64 @@
+for i in {1..10}
+do
+	node Regular.js false admitter 100 sidupRegular 20 &
+	node Regular.js false monitor 100 sidupRegular 20 &
+	node Regular.js false pi2 100 sidupRegular 20 &
+	node Regular.js false pi3 100 sidupRegular 20 &
+	node Regular.js false pi4 100 sidupRegular 20 &
+	node Regular.js false pi5 100 sidupRegular 20 &
+	node Regular.js false pi6 100 sidupRegular 20 &
+	node Regular.js false pi7 100 sidupRegular 20 &
+	node Regular.js false pi8 100 sidupRegular 20 &
+	node Regular.js false pi9 100 sidupRegular 20 &
+	node Regular.js false pi10 100 sidupRegular 20 &
+	node Regular.js false pi11 100 sidupRegular 20 &
+	node Regular.js false pi12 100 sidupRegular 20 &
+	node Regular.js false pi13 100 sidupRegular 20 &
+	node Regular.js false pi14 100 sidupRegular 20 &
+	node Regular.js false pi15 100 sidupRegular 20 &
+	node Regular.js false pi16 100 sidupRegular 20 &
+	node Regular.js false pi17 100 sidupRegular 20 &
+	node Regular.js false pi18 100 sidupRegular 20 &
+	node Regular.js false pi19 100 sidupRegular 20 &
+	node Regular.js false pi20 100 sidupRegular 20 &
+	node Regular.js false pi21 100 sidupRegular 20 &
+	node Regular.js false pi22 100 sidupRegular 20 &
+	node Regular.js false pi23 100 sidupRegular 20 &
+	node Regular.js false pi24 100 sidupRegular 20 &
+	node Regular.js false pi25 100 sidupRegular 20 &
+	node Regular.js false pi26 100 sidupRegular 20 &
+	node Regular.js false pi27 100 sidupRegular 20 &
+	node Regular.js false pi28 100 sidupRegular 20 &
+	node Regular.js false pi29 100 sidupRegular 20 &
+	node Regular.js false pi30 100 sidupRegular 20 &
+	node Regular.js false pi31 100 sidupRegular 20 &
+	node Regular.js false pi32 100 sidupRegular 20 &
+	node Regular.js false pi33 100 sidupRegular 20 &
+	node Regular.js false pi34 100 sidupRegular 20 &
+	node Regular.js false pi35 100 sidupRegular 20 &
+	node Regular.js false pi36 100 sidupRegular 20 &
+	node Regular.js false pi37 100 sidupRegular 20 &
+	node Regular.js false pi38 100 sidupRegular 20 &
+	node Regular.js false pi39 100 sidupRegular 20 &
+	node Regular.js false pi40 100 sidupRegular 20 &
+	node Regular.js false pi41 100 sidupRegular 20 &
+	node Regular.js false pi42 100 sidupRegular 20 &
+	node Regular.js false pi43 100 sidupRegular 20 &
+	node Regular.js false pi44 100 sidupRegular 20 &
+	node Regular.js false pi45 100 sidupRegular 20 &
+	node Regular.js false pi46 100 sidupRegular 20 &
+	node Regular.js false pi47 100 sidupRegular 20 &
+	node Regular.js false pi48 100 sidupRegular 20 &
+	node Regular.js false pi49 100 sidupRegular 20 &
+	node Regular.js false pi50 100 sidupRegular 20 &
+	node Regular.js false pi51 100 sidupRegular 20 &
+	node Regular.js false pi52 100 sidupRegular 20 &
+	node Regular.js false pi53 100 sidupRegular 20 &
+	node Regular.js false pi54 100 sidupRegular 20 &
+	node Regular.js false pi55 100 sidupRegular 20 &
+	node Regular.js false pi56 100 sidupRegular 20 &
+	node Regular.js false pi57 100 sidupRegular 20 &
+	node Regular.js false pi58 100 sidupRegular 20 &
+	node Regular.js false pi59 100 sidupRegular 20 
+	 sleep 5
+done
