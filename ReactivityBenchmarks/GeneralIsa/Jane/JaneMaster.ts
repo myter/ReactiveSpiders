d9d71a31acae791036c9ee2ba298c884035e8928
Florian Myter <fmyter@vub.ac.be>
2018-05-30 17:34:10 +0200

    Lowered sample size to test results quickly

--- ReactivityBenchmarks/GeneralIsa/Jane/JaneMaster.ts
+++ ReactivityBenchmarks/GeneralIsa/Jane/JaneMaster.ts
@@ -54,7 +54,8 @@ let csvFile         = process.argv[3]
 let dynamic         = process.argv[4] == "true"
 
 
-let allRates        = [10,100,200,300,400,500,600,700,800,900,1000]
+//let allRates        = [10,100,200,300,400,500,600,700,800,900,1000]
+let allRates        = [10,20,30,40,50,60,70,80,90,100]
 let allChanges      = [1,5,10,15,20]
 
 let master = new JaneMaster(thisIP,masterPort)
